27781f01e908a86c6b98a9afcf50400102d18224
Nelson Candanedo <nelson@candanedo.es>
2023-06-06 13:02:55 +0200

    Update run.sh
    Update to nfsen 1.3.8

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 # process config and install
 
-#cp /app/nfsen.conf /build/nfsen-1.3.7/etc/
-php /gen_conf.php > /build/nfsen-1.3.7/etc/nfsen.conf
-cd /build/nfsen-1.3.7
+#cp /app/nfsen.conf /build/nfsen-1.3.8/etc/
+php /gen_conf.php > /build/nfsen-1.3.8/etc/nfsen.conf
+cd /build/nfsen-1.3.8
 echo | ./install.pl etc/nfsen.conf
 
 # start nfsen
